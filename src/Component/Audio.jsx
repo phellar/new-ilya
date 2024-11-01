2d076ed622eaeac6d36545f3bcee30d56e10b579
phellar <sevenrosestech@yahoo.com>
2024-11-01 17:30:00 +0100

    update

--- src/Component/Audio.jsx
+++ src/Component/Audio.jsx
@@ -15,6 +15,10 @@ const Audio = () => {
                     <li><Link to="https://audiomack.com/mr-gyration-media-entertainment-international/song/i-love-kegite?share-user-id=142030352" target='blank'>I love kegite</Link></li>
                     <li><Link to="https://audiomack.com/mr-gyration-media-entertainment-international/album/agbaletu-2?share-user-id=142030352" target='blank'>Agbaletu</Link></li>
                     <li><Link to="https://audiomack.com/jayblaq-4/song/gbogbo-adaba128kbpsm4a" target='blank'>Gbogbo Adaba</Link></li>
+                    <li><Link to="https://audiomack.com/jagaban-lee/song/kegite-gyration-woro-2?share-user-id=68917189" target='blank'>Kegites Gyration Woro</Link></li>
+                    <li><Link to="https://audiomack.com/stephencoleman112/song/world-gyration-2324-jara" target='blank'>World Gyration</Link></li>
+                    <li><Link to="https://audiomack.com/yeyeogeimade/song/ariya-highlife" target='blank'>Ariya Highlife</Link></li>
+                    <li><Link to="https://audiomack.com/lov-gyration-africa/song/love-jara-2018-1" target='blank'>Love Jara</Link></li>
                 </ul>
             </div>
         </section>
